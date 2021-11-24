a711b83b56651081f464f9a772f45e86548119db
seongjin <sjan137@naver.com>
2021-11-24 18:54:16 +0900

    페이지 합치기

--- front_end/sj_pages/blog/src/App.js
+++ front_end/sj_pages/blog/src/App.js
@@ -1,41 +1,64 @@
 
+// import React, {useState} from 'react';
+
+// import './App.css';
+// import Header from './components/Header'
+// import Home from './components/Home';
+// import Flask from './components/Flask';
+// import { Router, Route, Routes, Link, BrowserRouter} from 'react-router-dom';
+// import Consum from './components/Consum';
+// import Culture from './components/Culture';
+// import Living from './components/Living';
+// import Metro from './components/Metro';
+
+
+// function App() {
+  
+//   return (
+//     <BrowserRouter>
+//       <div className="App">
+//         <Routes>
+//           <Route exact path="/" element={ <Home /> } />
+//           <Route exact path="/main" element={ <Header /> } />
+//           <Route exact path="/living" element={ <Header />, <Living />} />
+//           <Route exact path="/metro" element={<Metro />} />
+//           <Route exact path="/culture" element={<Culture />} />
+//           <Route exact path="/consum" element={<Consum />} />
+//         </Routes>
+        
+        
+        
+//         <Flask />
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+
+
+
+
+
+// export default App;
+
+/* eslint-disable */
+
 import React, {useState} from 'react';
 
+import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
-import Home from './components/Home';
+import Contents from './components/Contents';
 import Flask from './components/Flask';
-import { Router, Route, Routes, Link, BrowserRouter} from 'react-router-dom';
-import Consum from './components/Consum';
-import Culture from './components/Culture';
 import Living from './components/Living';
 import Metro from './components/Metro';
 
-
 function App() {
-  
   return (
-    <BrowserRouter>
       <div className="App">
-        <Routes>
-          <Route exact path="/" element={ <Home /> } />
-          <Route exact path="/main" element={ <Header /> } />
-          <Route exact path="/living" element={ <Header />, <Living />} />
-          <Route exact path="/metro" element={<Metro />} />
-          <Route exact path="/culture" element={<Culture />} />
-          <Route exact path="/consum" element={<Consum />} />
-        </Routes>
-        
-        
-        
+        <Header />
         <Flask />
       </div>
-    </BrowserRouter>
   );
 }
-
-
-
-
 
 export default App;
--- front_end/sj_pages/blog/src/components/Header.js
+++ front_end/sj_pages/blog/src/components/Header.js
@@ -1,6 +1,11 @@
 import React from 'react'
-import {Router, Route, Routes, Link} from 'react-router-dom';
-
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+import Consum from './Consum';
+import Contents from './Contents';
+import Culture from './Culture';
+import Home from './Home';
+import Living from './Living';
+import Metro from './Metro';
 
 
 const Header = () => {
@@ -17,34 +22,42 @@ const Header = () => {
 
   return (
     <>
-      <div className="navbar">
-        <div className="navbar_logo">
-          <i className="fas fa-horse"></i>
-          <a href="#">지피지기</a>
-        </div>
-        <div className="navbar_menu">
-          <li><Link to="/living">생활</Link></li>
-          <li><Link to="/metro">교통</Link></li>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/culture">문화</Link></li>
-          <li><Link to="/consum">소비</Link></li>
+      <Router>
+        <div className="navbar">
+          <div className="navbar_logo">
+            <i className="fas fa-horse"></i>
+            <a href="#">지피지기</a>
+          </div>
+          <div className="navbar_menu">
+            <li><Link to="/living">생활</Link></li>
+            <li><Link to="/metro">교통</Link></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/culture">문화</Link></li>
+            <li><Link to="/consum">소비</Link></li>
+            
+          </div>
           
-        </div>
-        
-        <div className="navbar_icons">
-          <select className="navbar_icons_options">
-            <option>국내</option>
-            <option>세계</option>
-          </select>
-          <li><a href="https://yeardream-gitlab.elice.io/yeardream-project/project-4" className="fab fa-gitlab"></a></li>
-          <li><i className="fas fa-bomb"></i></li>
-        </div>
+          <div className="navbar_icons">
+            <select className="navbar_icons_options">
+              <option>국내</option>
+              <option>세계</option>
+            </select>
+            <li><a href="https://yeardream-gitlab.elice.io/yeardream-project/project-4" className="fab fa-gitlab"></a></li>
+            <li><i className="fas fa-bomb"></i></li>
+          </div>
 
-        <a href="#" className="navbar_toggleBtn">
-          <i className="fas fa-bars"></i>
-        </a>
-      </div>
-      
+          <a href="#" className="navbar_toggleBtn">
+            <i className="fas fa-bars"></i>
+          </a>
+        </div>
+        <Routes>
+            <Route exact path="/living" element={<Living />} />
+            <Route exact path="/metro" element={<Metro />} />
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/culture" element={<Culture />} />
+            <Route exact path="/consum" element={<Consum />} />
+          </Routes>
+      </Router>
     </>
   )
 }
